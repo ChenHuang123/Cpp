0069debe107f6b2d1703ebe660feba5945ade7b5
PaulConradRCC <pjc@pauljconrad.net>
2016-03-21 15:28:37 -0700

    Cpp Template
    This file is the generic C++ file template for use in class.

---
+++ CppTemplate/cpp_template.cpp
@@ -0,0 +1,30 @@
+/*
+ *
+ *            PROGRAMMER: 
+ *         PROBLEM TITLE: 
+ *    PROBLEM DEFINITION: 
+ *                  DATE: 
+ *     SYSTEM - HARDWARE: 
+ *            - SOFTWARE: 
+ *         Input Devices: 
+ *        Output Devices: 
+ *                                                                          
+ *   PROGRAM ASSUMPTIONS: 
+ *                                                              
+ */
+
+// All necessary include directives here
+#include <iostream>
+
+// optional using name space std goes here
+using namespace std;
+
+// future (chap. 6+): function prototypes go here - delete this line if you are not using functions
+
+// main function here
+int main()
+{
+	// your main program goes here
+}
+
+// future (chap. 6+): function implementations go here - delete this line if you are not using functions
